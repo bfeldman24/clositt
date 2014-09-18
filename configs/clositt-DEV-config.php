ad0bf219c946d520ccdd3407a74f9b800c8fc9c0
Ben <ben@clositt.com>
2014-09-18 02:42:23 +0000

    Adding the db configs to the repo

---
+++ configs/clositt-DEV-config.php
@@ -0,0 +1,11 @@
+<?php
+
+$dsn = array(
+    'phptype'  => 'mysql',
+    'username' => 'bfish',
+    'password' => 'Cl0s1tt',
+    'hostspec' => 'localhost',
+    'database' => 'clositt_dev',
+);
+
+?>
---
+++ configs/clositt-config.php
@@ -0,0 +1,11 @@
+<?php
+
+$dsn = array(
+    'phptype'  => 'mysql',
+    'username' => 'root',
+    'password' => 'C1051ttUser',
+    'hostspec' => 'localhost',
+    'database' => 'thewinn2_clositt',
+);
+
+?>
