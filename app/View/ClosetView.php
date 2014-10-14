63787e89c391eb4beaf683c5779f7237e86627a6
Ben <ben@clositt.com>
2014-10-14 12:35:44 +0000

    removing debugging lines in the closetView

--- app/View/ClosetView.php
+++ app/View/ClosetView.php
@@ -1,7 +1,5 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
+require_once(dirname(__FILE__) . '/../globals.php');
 require_once(dirname(__FILE__) . '/ProductView.php');
 
 class ClosetView {	        
