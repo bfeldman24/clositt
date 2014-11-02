eb10ae8b8226a451d132e9dba5448988f53dd5d9
raphaelshorser <rshorser@gmail.com>
2014-11-01 23:30:25 -0400

    Merge branch 'master' of /Users/raphaelshorser/Work/clositt_new/clositt with conflicts.

--- configs/clositt-config.php
+++ configs/clositt-config.php
@@ -2,9 +2,9 @@
 
 $dsn = array(
     'phptype'  => 'mysql',
-    'username' => 'root',
+    'username' => 'thewinn2_clstusr',
     'password' => 'C1051ttUser',
-    'hostspec' => 'localhost',
+    'hostspec' => 'clositt.com',
     'database' => 'thewinn2_clositt',
 );
 
