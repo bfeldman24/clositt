1e09416e990d14cd78ec3f9982b58e7e4a0dd2a1
Ben <ben@clositt.com>
2014-11-14 13:31:59 +0000

    updating the google analytic link

--- www/static/footerMeta.php
+++ www/static/footerMeta.php
@@ -44,18 +44,18 @@ $(document).ready(function() {
         var r = d.getElementsByTagName(t)[0]; r.parentNode.insertBefore(s, r);
       }(document, 'script'));
     </script>
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-39518320-1']);
-      _gaq.push(['_trackPageview']);
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
     
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
+      ga('create', 'UA-39518320-1', 'auto');
+      ga('send', 'pageview');
+    
     </script>
 <?php } 
 
+global $mdb2;
 $mdb2->disconnect();
 ?>
