b8d5e1e796fcc67ba95958881a49ca565bd4d74f
Ben <BFeldman24@gmail.com>
2014-12-21 21:56:32 -0700

    updating the hangitt bookmarklet

--- www/js/hangitt.js.php
+++ www/js/hangitt.js.php
@@ -3,123 +3,45 @@ if (typeof $ == "function" && typeof $.noConflict == "function" && typeof $(docu
     $("scripts").remove();
 }
 
-if (typeof $ != "function" || 
-    typeof $(document).on != "function" || 
-    typeof $(document).off != "function" || 
-    typeof $('a').removeAttr != "function"){
+var hangitt = {
+    
+    init: function(){    
+        $(document).off("click","img", hangitt.showDialog);        
+        $(document).off("click","#hangitt-submit", hangitt.submit);        
+        $(document).off("click","#hangitt-cancel", hangitt.cancel);        
+        $(document).off("click","#close-itt", hangitt.close);
         
-    var jq = document.createElement('script');
-    jq.src = '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js';
-    document.documentElement.appendChild(jq);        
-    alert("Need to press the script 1 more times. Sorry");
+        $(document).on("click","img", hangitt.showDialog);        
+        $(document).on("click","#hangitt-submit", hangitt.submit);        
+        $(document).on("click","#hangitt-cancel", hangitt.cancel);        
+        $(document).on("click","#close-itt", hangitt.close);
+                
+        hangitt.initMouseEvents();        
+                
+        $(document).ready(hangitt.showBannerAndMask); 
+    },
+    
+    testJQuery: function(){         
+         
+         if (typeof $ != "function" || 
+             typeof $(document).on != "function" || 
+             typeof $(document).off != "function" || 
+             typeof $('a').removeAttr != "function"){
+                 
+             var jq = document.createElement('script');
+             
+             jq.src = '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js';
+             document.documentElement.appendChild(jq);        
 
-}else{   
+             return false;
+         } 
+         
+         return true;
+    },        
     
-    $(document).on("click","img",function(e){     
-        e.preventDefault();           
-        var src = $(this).attr("src");
-        var link = $(this).parents("a").first().attr("href");     
+    showBannerAndMask: function(e){
+        $("img").parents().css("z-index", "auto");
         
-        var $closets = $("<div>").attr("id","hangitt-dialog").attr("style","position: fixed; top: 20%; border: 1px solid rgb(204, 204, 204); background: none repeat scroll 0% 0% rgb(255, 255, 255); z-index: 30000; border-radius: 5px; padding: 10px; width: 450px; left: 35%;").append(
-            $("<div>").attr("style","font-size:28px;color:#999;font-weight:bold;text-align:center;").text("Add to your Clositt:")
-        );
-        
-        if (hangittClosets != null){
-            var $select = $("<select>").attr("style","margin: 20px 0; width: 100%");                
-                    
-            for (var i=0; i < hangittClosets.length; i++){
-                $select.append(            
-                    $("<option>").attr("value", hangittClosets[i].title)
-                                 .attr("number", hangittClosets[i].id)
-                                 .attr("style","margin: 5px 0; color: #999;font-size:14px;")
-                                 .text(hangittClosets[i].title)
-                );
-            }
-        }else{
-            var $select = $("<div>").text("You must create a clositt. Go to Clositt.com/myclositt");   
-        }
-        
-        var $img = $("<img>").attr("src",src).attr("link",link).attr("style","max-height: 250px;text-align:center; margin: 20px 20%;");
-        
-        var $cancel = $("<div>").addClass("hangitt-btn").attr("id","hangitt-cancel").attr("style","border: 1px solid rgb(170, 170, 170); border-radius: 4px; color: rgb(170, 170, 170); display: inline; padding: 8px 30px; margin: 0px 5px;cursor: pointer;").text("Cancel");
-        
-        var $submit = $("<div>").attr("id","hangitt-submit").attr("style","border: 1px solid #6cf; border-radius: 4px; color: #6cf; display: inline; padding: 8px 30px; margin: 0px 5px;cursor: pointer;").text("Submit");
-        
-        var $buttons = $("<div>").attr("style","margin: 20px auto;text-align:center").append($cancel).append($submit);
-        
-        $("body").append($closets.append($select).append($img).append($buttons));
-           
-        //alert("src: " + src + ". link: " + link + ". current: " + location.href);
-        //$("#close-itt").trigger("click");        
-        return false;
-    });
-    
-    $(document).on("click","#hangitt-submit", function(){
-        var closet = $("#hangitt-dialog > select > option:selected").first().attr("number");
-        var src = $("#hangitt-dialog img").first().attr("src");
-        var link = $("#hangitt-dialog img").first().attr("link");        
-        
-        var url = "http://clothies.bprowd.com/hangitt/<?php echo $_GET['user']; ?>/"+closet;
-        url += "?src=" + src.replace(/\?/g, '~~').replace(/\&/g, '~~~');
-        
-        if (link != null){
-            url += "&link=" + link.replace(/\?/g, '~~').replace(/\&/g, '~~~');
-        }
-        
-        url += "&page=" + location.href.replace(/\?/g, '~~').replace(/\&/g, '~~~');
-        
-        $("#hangitt-dialog img").first().attr("src", url);
-        
-        $("#hangitt-cancel").text("Ok");
-        $("#hangitt-dialog select").remove();
-        $(this).remove();                
-    });
-    
-    $(document).on("click","#hangitt-cancel", function(){
-        $("#hangitt-dialog").remove();        
-    });
-    
-    $(document).on("click","#close-itt", function(){
-        $("#hangitt").remove();
-        $("#hangitt-mask").remove();
-        $(document).off("click","img");
-        $("img").css("z-index", "1").css("position", "inherit").css("cursor","auto");
-    });                
-    
-    $(document).on("mouseenter", ".hangitt-btn", function(){
-        $(this).css("border","1px solid #999").css("color","#999");
-    });
-
-    $(document).on("mouseleave", ".hangitt-btn", function(){
-        $(this).css("border","1px solid #CCC").css("color","#CCC").css("background-color","#FFF");
-    });
-    
-    $(document).on("mousedown", ".hangitt-btn", function(){
-        $(this).css("background-color","#F5F5F5");
-    });  
-    
-    $(document).on("mouseup", ".hangitt-btn", function(){
-        $(this).css("background-color","#FFF");
-    });    
-    
-    $(document).on("mouseenter", "#hangitt-submit", function(){
-        $(this).css("border","1px solid #4ad").css("color","#4ad");
-    });
-    
-    $(document).on("mouseleave", "#hangitt-submit", function(){
-        $(this).css("border","1px solid #6cf").css("color","#6cf").css("background-color","#FFF");
-    });
-    
-    $(document).on("mousedown", "#hangitt-submit", function(){
-        $(this).css("background-color","#E0FFFF");
-    });  
-    
-    $(document).on("mouseup", "#hangitt-submit", function(){
-        $(this).css("background-color","#FFF");
-    });
-            
-    $(document).ready(function(){        
-    
         /* show top panel */                
         $("body").append(
             $("<div>").attr("id","hangitt").attr("style","position: fixed !important;z-index:9999999 !important;top:0px !important;left:0px !important;right:0px !important;width:100% !important;height:100px !important;background:#FFFFFF !important;border-bottom:5px solid #6cf !important;font-size:14px !important;color:#333 !important")
@@ -135,12 +57,132 @@ if (typeof $ != "function" ||
         );
         
         $("img").css("z-index", "20000").css("position", "relative").css("cursor","pointer");
-    });          
+    },
+    
+    showDialog: function(e){
+        e.preventDefault();           
+        var src = $(this).attr("src");
+        var link = $(this).parents("a").first().attr("href");     
+        
+        var $closets = $("<div>").attr("id","hangitt-dialog").attr("style","position: fixed; top: 20%; border: 1px solid rgb(204, 204, 204); background: none repeat scroll 0% 0% rgb(255, 255, 255); z-index: 30000; border-radius: 5px; padding: 10px; width: 450px; left: 35%;").append(
+            $("<div>").attr("style","font-size:28px;color:#999;font-weight:bold;text-align:center;").text("Add to your Clositt:")
+        );
+        
+        if (hangittClosets != null){
+            var $select = $("<select>").attr("style","margin: 20px 0; width: 100%");                
+                    
+            for (var i=0; i < hangittClosets.length; i++){
+                $select.append(            
+                    $("<option>").attr("value", hangittClosets[i].title)
+                                    .attr("number", hangittClosets[i].id)
+                                    .attr("style","margin: 5px 0; color: #999;font-size:14px;")
+                                    .text(hangittClosets[i].title)
+                );
+            }
+        }else{
+            var $select = $("<div>").text("You must create a clositt. Go to Clositt.com/myclositt");   
+        }
+        
+        var $img = $("<img>").attr("src",src).attr("link",link).attr("style","max-height: 250px;text-align:center; margin: 20px 20%;");
+        
+        var $cancel = $("<div>").addClass("hangitt-btn").attr("id","hangitt-cancel").attr("style","border: 1px solid rgb(170, 170, 170); border-radius: 4px; color: rgb(170, 170, 170); display: inline; padding: 8px 30px; margin: 0px 5px;cursor: pointer;").text("Cancel");
+        
+        var $submit = $("<div>").attr("id","hangitt-submit").attr("style","border: 1px solid #6cf; border-radius: 4px; color: #6cf; display: inline; padding: 8px 30px; margin: 0px 5px;cursor: pointer;").text("Submit");
+        
+        var $buttons = $("<div>").attr("style","margin: 20px auto;text-align:center").append($cancel).append($submit);
+        
+        $("body").append($closets.append($select).append($img).append($buttons));
+            
+        //alert("src: " + src + ". link: " + link + ". current: " + location.href);
+        //$("#close-itt").trigger("click");        
+        return false;
+    },
+    
+    initMouseEvents: function(){
+        // grey buttons
+        $(document).on("mouseenter", ".hangitt-btn", function(){
+            $(this).css("border","1px solid #999").css("color","#999");
+        });    
+        $(document).on("mouseleave", ".hangitt-btn", function(){
+            $(this).css("border","1px solid #CCC").css("color","#CCC").css("background-color","#FFF");
+        });
+                
+        $(document).on("mousedown", ".hangitt-btn", function(){
+            $(this).css("background-color","#F5F5F5");
+        });  
+        
+        $(document).on("mouseup", ".hangitt-btn", function(){
+            $(this).css("background-color","#FFF");
+        });    
+        
+        // blue buttons
+        $(document).on("mouseenter", "#hangitt-submit", function(){
+            $(this).css("border","1px solid #4ad").css("color","#4ad");
+        });
+        
+        $(document).on("mouseleave", "#hangitt-submit", function(){
+            $(this).css("border","1px solid #6cf").css("color","#6cf").css("background-color","#FFF");
+        });
+        
+        $(document).on("mousedown", "#hangitt-submit", function(){
+            $(this).css("background-color","#E0FFFF");
+        });  
+        
+        $(document).on("mouseup", "#hangitt-submit", function(){
+            $(this).css("background-color","#FFF");
+        });
+    },
+    
+    submit: function(e){
+        var closet = $("#hangitt-dialog > select > option:selected").first().attr("number");
+        var src = $("#hangitt-dialog img").first().attr("src");
+        var link = $("#hangitt-dialog img").first().attr("link");        
+        
+        if (closet != null && src != null){        
+            var url = "http://clothies.bprowd.com/hangitt/<?php echo $_GET['user']; ?>/"+closet;
+            url += "?src=" + src.replace(/\?/g, '~~').replace(/\&/g, '~~~');
+            
+            if (link != null){
+                url += "&link=" + link.replace(/\?/g, '~~').replace(/\&/g, '~~~');
+            }
+            
+            url += "&page=" + location.href.replace(/\?/g, '~~').replace(/\&/g, '~~~');
+            
+            $("#hangitt-dialog img").first().attr("src", url);
+            $("#hangitt-dialog img").on("load", function(){
+                $("#hangitt-cancel").text("Ok");     
+                $("#hangitt-dialog img").off("load");
+            });
+            
+            $("#hangitt-cancel").text("Saving...");
+            $("#hangitt-dialog select").remove();
+            $(this).remove(); 
+        }
+    },
+    
+    cancel: function(e){
+         $("#hangitt-dialog").remove();   
+    },
+    
+    close: function(e){
+        $("#hangitt").remove();
+        $("#hangitt-mask").remove();
+        $(document).off("click","img");
+        $("img").css("z-index", "1").css("position", "inherit").css("cursor","auto");
+    }
+}         
+
+if(hangitt.testJQuery()){
+    hangitt.init();
+    console.log("fast start");
+}else{
+    setTimeout(hangitt.init, 1000); 
+    console.log("starting..."); 
 }
 
 <?php
-require_once(dirname(__FILE__) . '/../app/session.php');
-require_once(dirname(__FILE__) . '/../app/Controller/ClosetController.php');
+require_once(dirname(__FILE__) . '/../../app/session.php');
+require_once(dirname(__FILE__) . '/../../app/Controller/ClosetController.php');
 
 $closetController = new ClosetController();  
 $result = $closetController->getAllClosets(true, $_GET['user']);
