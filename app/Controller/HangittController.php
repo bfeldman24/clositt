99ac48af3f6edd1a4b441ecf990f39077b45705d
Ben <ben@clositt.com>
2015-01-11 19:43:18 +0000

    making modifications for hangitt extension

--- app/Controller/HangittController.php
+++ app/Controller/HangittController.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
 require_once(dirname(__FILE__) . '/../session.php');
 require_once(dirname(__FILE__) . '/ClosetController.php');
 
@@ -16,12 +13,13 @@ class HangittController{
             $userid = $_GET['user'];   
         }else if (isset($_SESSION['active']) && $_SESSION['active'] === true && 
                   isset($_SESSION['userid']) && is_numeric($_SESSION['userid'])){
-            $userid = $_GET['userid'];               
+            $userid = $_SESSION['userid'];               
         }  
         
         if ($userid > 0){
             $closetController = new ClosetController();  
-            $result['success'] = $closetController->getAllClosets(true, $userid);
+            $result['user'] = $userid;
+	    $result['success'] = $closetController->getAllClosets(false, $userid);
         }else{
             $result['error'] = "Need to Sign in!";   
         }
